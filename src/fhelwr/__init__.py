c8b97f050690bdf90ef8ea8931592ee5fa13bdc8
marcosfpr <mfprezende@gmail.com>
2024-07-07 14:00:30 -0300

    first commit

---
+++ src/fhelwr/__init__.py
@@ -0,0 +1,10 @@
+"""
+FHElwr is a flwr extension for Federated Learning with encrypted weights
+aggregated homomorphically.
+"""
+
+from fhelwr import clients, strategies
+
+VERSION = "0.0.1"
+
+__all__ = ["clients", "strategies"]
---
+++ src/fhelwr/client.py
@@ -0,0 +1,212 @@
+import logging
+from abc import ABC, abstractmethod
+from typing import Any, Dict, List, Tuple
+
+import flwr as fl
+import numpy as np
+from flwr.common import (Code, EvaluateIns, EvaluateRes, FitIns, FitRes,
+                         GetParametersIns, GetParametersRes, Scalar, Status)
+from sealy import (BatchDecryptor, BatchEncryptor, CiphertextBatchArray,
+                   CKKSBatchEncoder, Context, KeyGenerator)
+
+from fhelwr.model import (ciphertext_to_params, flatten_parameters,
+                          params_to_ciphertext, unflatten_parameters)
+
+
+class SealyScheme(ABC):
+    """
+    The encryption schemes supported by SEALY.
+    """
+    ...
+
+class CKKS(SealyScheme):
+    """
+    The CKKS encryption scheme.
+    """
+    def __init__(self, scale: float) -> None:
+        self.scale = scale
+
+
+class SealyClient(fl.client.Client, ABC):
+    """
+    Flower client that uses SEALY to encrypt the model parameters.
+    """
+
+    def __init__(self, cid: int, context: Context, scheme: SealyScheme) -> None:
+        """
+        Initialize the client with the neural network and data loaders.
+
+        Args:
+            cid: Client ID
+            context: SEALY context
+        """
+        self.cid = cid
+        self.ctx = context
+        self.scheme = scheme
+        self.keygen = KeyGenerator(self.ctx)
+
+    @abstractmethod
+    def get_net(self) -> Any:
+        """
+        Return the neural network used by this client.
+        """
+        pass
+    
+    @abstractmethod
+    def set_parameters(self, net, parameters: List[np.ndarray]) -> None:
+        """
+        Load the parameters into the neural network.
+        """
+        pass
+    
+    @abstractmethod
+    def train(self, net) -> int:
+        """
+        Train the neural network.
+
+        Returns:
+            int: The number of examples used for training.
+        """
+        pass
+
+    @abstractmethod
+    def test(self, net) -> Tuple[int, float, Dict[str, Scalar]]:
+        """
+        Test the neural network.
+
+        Returns:
+            int: The number of examples used for testing.
+            float: The loss.
+            dict: The metrics.
+        """
+        pass
+
+
+    def __encrypt_params(self, parameters: np.ndarray) -> CiphertextBatchArray:
+        """
+        Get the parameters of the neural network as an encrypted tensor.
+        """
+        encoded = self.__get_encoder().encode(parameters.tolist())
+        encrypted = self.__get_encryptor().encrypt(encoded)
+        return encrypted
+
+    def __decrypt_params(self, encrypted: CiphertextBatchArray) -> np.ndarray:
+        decrypted = self.__get_decryptor().decrypt(encrypted)
+        decoded = np.asarray(self.__get_encoder().decode(decrypted))
+        return decoded
+
+    def __get_encoder(self) -> CKKSBatchEncoder:
+        """
+        Return the encoder used by this client.
+        """
+        if isinstance(self.scheme, CKKS):
+            return CKKSBatchEncoder(self.ctx, self.scheme.scale) 
+        else:
+            raise ValueError(f"Unsupported scheme: {self.scheme}")
+
+    def __get_encryptor(self) -> BatchEncryptor:
+        """
+        Return the encryptor used by this client.
+        """
+        return BatchEncryptor(self.ctx, self.keygen.create_public_key())
+
+    def __get_decryptor(self) -> BatchDecryptor:
+        """
+        Return the decryptor used by this client.
+        """
+        return BatchDecryptor(self.ctx, self.keygen.secret_key())
+
+
+    def get_parameters(self, ins: GetParametersIns) -> GetParametersRes:
+        logging.info("Client %d: get_parameters", self.cid)
+
+        net = self.get_net()
+        net_parameters = flatten_parameters(net)
+
+        # Get parameters as encrypted tensor
+        logging.debug("Encrypting parameters for client %d", self.cid)
+        encrypted_parameters = self.__encrypt_params(net_parameters)
+        logging.debug("Encrypted parameters for client %d", self.cid)
+
+        # Serialize to bytes
+        logging.debug("Serializing encrypted parameters for client %d", self.cid)
+        parameters = ciphertext_to_params(encrypted_parameters)
+        logging.debug("Serialized encrypted parameters for client %d", self.cid)
+
+        # Status
+        status = Status(code=Code.OK, message="Success")
+
+        return GetParametersRes(parameters=parameters, status=status)
+
+
+    def fit(self, ins: FitIns) -> FitRes:
+        logging.info("Client %d: fit", self.cid)
+
+        ciphered_parameters = ins.parameters
+
+        parameters = params_to_ciphertext(self.ctx, ciphered_parameters)
+        # Decrypt parameters
+        decrypted_parameters = self.__decrypt_params(parameters)
+
+        # Unflatten parameters
+        net = self.get_net()
+        unflat_params = unflatten_parameters(net, decrypted_parameters)
+
+        # Set parameters
+        logging.debug("Setting parameters for client %d", self.cid)
+        self.set_parameters(net, unflat_params)
+
+        # Train
+        logging.debug("Training for client %d", self.cid)
+        num_examples = self.train(net)
+        logging.debug("Trained for client %d", self.cid)
+
+        # Get updated parameters
+        logging.debug("Getting parameters for client %d", self.cid)
+        updated_parameters = flatten_parameters(net)
+        logging.debug("Got parameters for client %d", self.cid)
+
+        # Encrypt parameters
+        logging.debug("Encrypting parameters for client %d", self.cid)
+        encrypted_parameters = self.__encrypt_params(updated_parameters)
+        logging.debug("Encrypted parameters for client %d", self.cid)
+
+        # Serialize to bytes
+        logging.debug("Serializing encrypted parameters for client %d", self.cid)
+        parameters = ciphertext_to_params(encrypted_parameters)
+        logging.debug("Serialized encrypted parameters for client %d", self.cid)
+
+        # Status
+        status = Status(code=Code.OK, message="Success")
+
+        return FitRes(parameters=parameters, num_examples=num_examples, status=status, metrics={})
+
+    def evaluate(self, ins: EvaluateIns) -> EvaluateRes:
+        logging.info("Client %d: evaluate", self.cid)
+
+        ciphered_parameters = ins.parameters
+
+        # Decrypt parameters
+        parameters = params_to_ciphertext(self.ctx, ciphered_parameters)
+        logging.debug("Decrypting parameters for client %d", self.cid)
+        decrypted_parameters = self.__decrypt_params(parameters)
+        logging.debug("Decrypted parameters for client %d", self.cid)
+
+        # Unflatten parameters
+        net = self.get_net()
+        unflat_params = unflatten_parameters(net, decrypted_parameters)
+
+
+        # Set parameters
+        logging.debug("Setting parameters for client %d", self.cid)
+        self.set_parameters(net, unflat_params)
+        logging.debug("Set parameters for client %d", self.cid)
+
+
+        logging.debug("Testing for client %d", self.cid)
+        num_examples, loss, metrics = self.test(net)
+        logging.debug("Tested for client %d", self.cid)
+
+        # Status
+        status = Status(code=Code.OK, message="Success")
+        return EvaluateRes(loss=loss, num_examples=num_examples, status=status, metrics=metrics)
